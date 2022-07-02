4a73d60c198ce8b3b0f9444a48102e9d3e9076d6
ColorfulQuark <108406142+ColorfulQuark@users.noreply.github.com>
2022-07-02 09:41:46 -0400

    Update higher-of-two-rolls.py

--- higher-of-two-rolls.py
+++ higher-of-two-rolls.py
@@ -1,15 +1,8 @@
-import random
+from random import randint
+from statistics import mean
 
 number_of_sides  =  int(input("How many sides on your dice? "))
 
-sum_of_results = 0.0
+avg =  mean([max(randint(1,number_of_sides), randint(1,number_of_sides)) for _ in range(10**6)])
 
-trials = 0
-while trials < 10**6:
-    sum_of_results += max([int(random.random()*number_of_sides)+1,int(random.random()*number_of_sides)+1])
-    trials += 1
-
-print("Average result of rolling two and taking the highest is about {0}".format(sum_of_results/trials))
-
-
-
+print(f"Average result of rolling two and taking the highest is about {avg}")
