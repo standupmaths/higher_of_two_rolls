2b9d28835ceb0de43ae6e02f2f54c40736becc44
Kerbaltec-Solutions <61379284+Kerbaltec-Solutions@users.noreply.github.com>
2022-08-13 15:37:24 +0200

    dive v1.2
    Now added support for up to four dice and also to set the minimum number (ie. select 0 to simulate coin flips and 1 for dice rolls)

--- dice.py
+++ dice.py
@@ -3,14 +3,23 @@ import math
 
 number_of_side1  =  int(input("How many sides on dice 1? "))
 number_of_side2  =  int(input("How many sides on dice 2? "))
+number_of_side3  =  int(input("How many sides on dice 3? "))
+number_of_side4  =  int(input("How many sides on dice 4? "))
+add = int(input("minimum: 0/1 "))
+
+add1=add*min([1,number_of_side1])
+add2=add*min([1,number_of_side2])
+add3=add*min([1,number_of_side3])
+add4=add*min([1,number_of_side4])
+
 print()
 print("Input rolling method")
 print("0: single dice")
-print("1: highest of two")
-print("2: lowest of two")
-print("3: sum of two")
-print("4: difference of two")
-print("5: product of two")
+print("1: highest of four")
+print("2: lowest of four")
+print("3: sum of four")
+print("4: difference of four")
+print("5: product of four")
 method=int(input())
 print()
 exp=int(input("Input number of dice rolls 10^"))
@@ -23,17 +32,17 @@ result = 0
 trials = 0
 while trials < 10**exp:
 	if method==1:
-		result = max([int(random.random()*number_of_side1)+1,int(random.random()*number_of_side2)+1])
+		result = max([int(random.random()*number_of_side1)+add1,int(random.random()*number_of_side2)+add2,int(random.random()*number_of_side3)+add3,int(random.random()*number_of_side4)+add4])
 	elif method==2:
-		result = min([int(random.random()*number_of_side1)+1,int(random.random()*number_of_side2)+1])
+		result = min([int(random.random()*number_of_side1)+add1,int(random.random()*number_of_side2)+add2,int(random.random()*number_of_side3)+add3,int(random.random()*number_of_side4)+add4])
 	elif method==3:
-		result = (int(random.random()*number_of_side1)+1)+(int(random.random()*number_of_side2)+1)
+		result = (int(random.random()*number_of_side1)+add1)+(int(random.random()*number_of_side2)+add2)+(int(random.random()*number_of_side3)+add3)+(int(random.random()*number_of_side4)+add4)
 	elif method==4:
-		result = abs((int(random.random()*number_of_side1)+1)-(int(random.random()*number_of_side2)+1))
+		result = abs((int(random.random()*number_of_side1)+add1)-(int(random.random()*number_of_side2)+add2)-(int(random.random()*number_of_side3)+add3)-(int(random.random()*number_of_side4)+add4))
 	elif method==5:
-		result = (int(random.random()*number_of_side1)+1)*(int(random.random()*number_of_side2)+1)
+		result = (int(random.random()*number_of_side1)+add1)*(int(random.random()*number_of_side2)+add2)*(int(random.random()*number_of_side3)+add3)*(int(random.random()*number_of_side4)+add4)
 	else:
-		result = int(random.random()*number_of_side1)+1
+		result = int(random.random()*number_of_side1)+add1
 	
 	maxi = max(maxi,result)
 	sum_of_results += result
